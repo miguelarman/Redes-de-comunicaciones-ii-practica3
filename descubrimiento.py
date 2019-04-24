60c84ab8b09b9b892d96284bfb91efa763d0e682
Miguel Arconada Manteca <miguel.arconada@estudiante.uam.es>
2019-04-24 14:39:36 +0200

    Refactorizado la parte de SD con clase

--- descubrimiento.py
+++ descubrimiento.py
@@ -4,93 +4,103 @@ import sys
 server_url = 'vega.ii.uam.es'
 server_port = 8000
 
-def connect_to_server():
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class DS_connection:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.socket = None
 
-    # Connect the socket to the port where the server is listening
-    server_address = (server_url, server_port)
-    sock.connect(server_address)
+    def connect_to_server(self):
+        # Create a TCP/IP
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    return sock
+        # Connect the socket to the port where the server is
+        server_address = (self.ip, self.port)
+        self.socket.connect(server_address)
 
-def register(sock, nick, ip, port, password, protocol):
-    command = 'REGISTER ' + nick + ' ' + ip + ' ' + str(port) + ' ' + password + ' ' + protocol
-    sock.sendall(command.encode())
-    data = sock.recv(4096).decode()
 
-    if (data[0:2] != 'OK'):
-        print('Error con la peticion')
-        print(data)
-        return
+    def register(self, nick, ip, port, password, protocol):
+        command = 'REGISTER ' + nick + ' ' + ip + ' ' + str(port) + ' ' + password + ' ' + protocol
+        self.socket.sendall(command.encode())
+        data = self.socket.recv(4096).decode()
 
-    ts = data.split(' ')[3]
+        if (data[0:2] != 'OK'):
+            print('Error con la peticion')
+            print(data)
+            return
 
-    return ts
+        ts = data.split(' ')[3]
 
-def list_users(sock):
-    command = 'LIST_USERS'
-    sock.sendall(command.encode())
-    data = sock.recv(4096).decode()
+        return ts
 
-    ret = data.split(' ')
-    if (ret[0] != 'OK'):
-        print('Error con la peticion')
-        print(data)
-        return
+    def list_users(self):
+        command = 'LIST_USERS'
+        self.socket.sendall(command.encode())
+        data = self.socket.recv(4096).decode()
 
-    num = ret[2]
-    list = data[len('OK USERS_LIST ') + len(str(num)) + 1:]
-    list = list.split('#')
+        ret = data.split(' ')
+        if (ret[0] != 'OK'):
+            print('Error con la peticion')
+            print(data)
+            return
 
-    array = []
-    for entry in list:
-        [name, ip, port, time] = entry.split(' ')
+        num = ret[2]
+        list = data[len('OK USERS_LIST ') + len(str(num)) + 1:]
+        list = list.split('#')
+
+        array = []
+        for entry in list:
+            if len(entry) == 0:
+                continue
+
+            [name, ip, port, time] = entry.split(' ')
+
+            user = {
+            'name': name,
+            'ip': ip,
+            'port': port,
+            'time': time
+            }
+            array.append(user)
+
+        return array
+
+    def query(self, nick):
+        command = 'QUERY ' + nick
+        self.socket.sendall(command.encode())
+        data = self.socket.recv(4096).decode()
+
+        if (data[0:2] != 'OK'):
+            print('Error con la peticion')
+            print(data)
+            return
+
+        user = data[len('OK USER_FOUND '):]
+
+        [name, ip, port, version] = user.split(' ')
 
         user = {
         'name': name,
         'ip': ip,
         'port': port,
-        'time': time
+        'version': version
         }
-        array.append(user)
 
-    return array
+        return user
 
-def query(sock, nick):
-    command = 'QUERY ' + nick
-    sock.sendall(command.encode())
-    data = sock.recv(4096).decode()
+    def quit(self):
+        command = 'QUIT'
+        self.socket.sendall(command.encode())
+        data = self.socket.recv(4096).decode()
+        self.socket.close()
 
-    if (data[0:2] != 'OK'):
-        print('Error con la peticion')
-        print(data)
-        return
+        return data
 
-    user = data[len('OK USER_FOUND '):]
+obj = DS_connection(server_url, server_port)
+obj.connect_to_server()
 
-    [name, ip, port, version] = user.split(' ')
-
-    user = {
-    'name': name,
-    'ip': ip,
-    'port': port,
-    'version': version
-    }
-
-    return user
-
-def quit(sock):
-    command = 'QUIT'
-    sock.sendall(command.encode())
-    data = sock.recv(4096).decode()
-
-    return data
-
-sock = connect_to_server()
-
-# data = list_users(sock)
-# data = query(sock, 'mam')
-# data = register(sock, 'mam', '127.0.0.1', 9999, 'password', 'V0')
-data = quit(sock)
+# data = obj.list_users()
+# data = obj.query('mam')
+data = obj.register('mam', '127.0.0.1', 9999, 'password', 'V0')
+# data = obj.quit()
 print(data)
