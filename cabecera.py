9c054dee79abc8147844e0c2f30438b775b3165b
Mario García Pascual <mario.garciapascual@estudiante.uam.es>
2019-05-06 19:20:53 +0200

    añadidos comentarios a cabecera

--- cabecera.py
+++ cabecera.py
@@ -1,14 +1,49 @@
 import time
+from parse import parse
 
 class Cabecera:
+    """
+        Esta clase permite poner y quitar
+        las cabeceras para el control de
+        las comunicaciones
+    """
     counter = 0
 
     def resetCounter():
+        """ Resetea el contador de paquetes
+
+        Returns
+        -------
+        type
+            None
+
+        """
         Cabecera.counter = 0
         return
 
-    # datos esta representado en bytes
+
     def poner(ancho, alto, fps, datos):
+        """
+            Pone la cabecera con la informacion necesaria a los
+            datos pasados, que estan representados en bytes
+
+        Parameters
+        ----------
+        ancho : int
+            El ancho de la resolucion
+        alto : int
+            El alto de la resolucion
+        fps : int
+            Los fps que se estan retransmitiendo
+        datos : bytes
+            Los datos en bytes
+
+        Returns
+        -------
+        bytes
+            Los bytes correspondientes a la cabecera concatenado a los datos
+
+        """
         # numero de orden de paquete
         ord = Cabecera.counter
         Cabecera.counter += 1
@@ -21,7 +56,22 @@ class Cabecera:
         return header + datos
 
     def quitar(msg):
-        foo = msg.decode().split('#')
+        """
+            Quita la cabecera al paquete y devuelve la informacion
+            parseada junto con los datos
+
+        Parameters
+        ----------
+        msg : bytes
+            El paquete al que se le quiere quitar la cabecera
+
+        Returns
+        -------
+        dict
+            Diccionario con la informacion de la cabecera y los datos
+
+        """
+        foo = parse('{}#{}#{}#{}#{}', msg.decode())
         return {
             'ord' : foo[0],
             'ts' : foo[1],
