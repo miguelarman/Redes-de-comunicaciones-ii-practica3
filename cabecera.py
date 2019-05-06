58376113f929972c7a013bafab7e08a22ecc7140
Mario García Pascual <mario.garciapascual@estudiante.uam.es>
2019-05-06 02:56:38 +0200

    cambios en clase cabecera

--- cabecera.py
+++ cabecera.py
@@ -7,6 +7,7 @@ class Cabecera:
         Cabecera.counter = 0
         return
 
+    # datos esta representado en bytes
     def poner(ancho, alto, fps, datos):
         # numero de orden de paquete
         ord = Cabecera.counter
@@ -15,14 +16,16 @@ class Cabecera:
         # timestamp
         ts = int(time.time())
 
-        return "{}#{}#{}x{}#{}#{}".format(ord, ts, ancho, alto, fps, datos)
+        header = "{}#{}#{}x{}#{}#".format(ord, ts, ancho, alto, fps).encode()
+
+        return header + datos
 
     def quitar(msg):
-        foo = msg.split('#')
+        foo = msg.decode().split('#')
         return {
             'ord' : foo[0],
             'ts' : foo[1],
             'res' : foo[2],
             'fps' : foo[3],
-            'datos' : foo[4]
+            'datos' : foo[4].encode()
         }
