07752345d2456eba6966b6ea390bc1ced94b20c6
Mario García Pascual <mario.garciapascual@estudiante.uam.es>
2019-05-10 18:17:22 +0200

    fichero de la aplicacion, falta la parte de la gui

---
+++ version2/aplicacion.py
@@ -0,0 +1,127 @@
+import threading
+import collections
+import random
+
+from modulo_tcp import ModuloTCP
+from conexion_ds import ConexionDS
+from generales import *
+
+class App:
+    nick = None
+    ip = None
+    my_tcp_port = None
+    my_udp_port = None
+    protocol = None
+
+    my_tcp_addr = None
+    my_udp_addr = None
+
+    logged = False
+    on_call = False
+    on_hold = False
+
+    on_call_lock = threading.Lock()
+
+    gui = None
+    tcp = None
+    control_conn = None
+
+    in_buf = collections.deque()
+    out_buf = collections.deque()
+
+    def login(nick, ip, port, pwd, proto):
+        # hacemos uso del servidor de descubrimiento
+        ds = ConexionDS()
+        foo = ds.register(nick, ip, port, pwd, proto)
+        if not foo:
+            print("login fallido")
+        else:
+            print("login exitoso")
+
+            App.nick = nick
+            App.ip = ip
+            App.my_tcp_port = port
+            App.my_udp_port = random.randint(1000, 60000)
+            App.protocol = proto
+
+            App.my_tcp_addr = (App.ip, int(App.my_tcp_port))
+            App.my_udp_addr = (App.ip, int(App.my_udp_port))
+
+            App.tcp = ModuloTCP()
+            App.tcp.start()
+
+            App.logged = True
+
+            return True
+
+    def __llamar(her_tcp_addr):
+        return App.tcp.llamar(her_tcp_addr)
+
+    def llamar(nick):
+        if App.logged:
+            ds = ConexionDS()
+            foo = ds.query(nick)
+            if not foo:
+                print("nick no encontrado")
+            else:
+                print("usuario encontrado")
+                ret = App.__llamar((foo['ip'], int(foo['port'])))
+                if not ret:
+                    print("no se ha podido establecer llamada")
+                else:
+                    print("en llamada")
+                    return True
+
+    def responder(nick):
+        print('Responde y para aceptar y cualquier otra cosa para denegar')
+        ans = input('Llamada recibida de {}'.format(nick))
+        if ans == 'y':
+            return True
+        else:
+            return False
+
+    def pausar():
+        if App.on_call:
+            App.control_conn.pausar()
+            App.on_hold = True
+            return True
+
+    def nos_pausan():
+        if App.on_call:
+            App.on_hold = True
+            return True
+
+
+    def reanudar():
+        if App.on_call:
+            App.control_conn.reanudar()
+            App.on_hold = False
+            return True
+
+    def nos_reanudan():
+        if App.on_call:
+            App.on_hold = False
+            return True
+
+    def colgar():
+        if App.on_call:
+            App.control_conn.colgar()
+            App.control_conn.stop()
+            App.on_hold = False
+            App.on_call = False
+            return True
+
+    def nos_cuelgan():
+        if App.on_call:
+            App.control_conn.stop()
+            App.on_hold = False
+            App.on_call = False
+
+    def stop():
+        if App.logged:
+            if App.on_call:
+                App.colgar()
+            App.tcp.shutdown()
+            return
+        else:
+            return
