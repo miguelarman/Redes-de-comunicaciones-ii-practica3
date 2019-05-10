5e9daeab3bf77e8cce8aeef0d7784da67d47559d
Mario García Pascual <mario.garciapascual@estudiante.uam.es>
2019-05-10 18:19:58 +0200

    conexion al servidor de descubrimiento

---
+++ version2/conexion_ds.py
@@ -0,0 +1,48 @@
+import socket
+
+from generales import *
+
+class ConexionDS:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect(DS_ADDR)
+
+    def register(self, nick, ip, port, pwd, proto):
+        cmd = 'REGISTER {} {} {} {} {}'.format(nick, ip, port, pwd, proto)
+        self.sock.sendall(cmd.encode())
+        data = self.sock.recv(BUFSIZE).decode()
+
+        foo = data.split(' ')
+        if foo[0] == 'OK':
+            return {'nick':foo[2], 'ts':foo[3]}
+        else:
+            return None
+
+    def query(self, nick):
+        cmd = 'QUERY {}'.format(nick)
+        self.sock.sendall(cmd.encode())
+        data = self.sock.recv(BUFSIZE).decode()
+
+        foo = data.split(' ')
+        if foo[0] == 'OK':
+            return {'nick':foo[2], 'ip':foo[3], 'port':foo[4], 'proto':foo[5]}
+        else:
+            return None
+
+    def list_users(self):
+        cmd = 'LIST_USERS'
+        self.sock.sendall(cmd.encode())
+        data = self.sock.recv(BUFSIZE).decode()
+
+        foo = data.split(' ')
+        if foo[0] == 'OK':
+            return ' '.join(foo[3:]).split('#')
+        else:
+            return None
+
+    def quit(self):
+        cmd = 'QUIT'
+        self.sock.sendall(cmd.encode())
+        data = self.sock.recv(BUFSIZE).decode()
+
+        return data
