7302e698485dc0dca077053fdd7ca2422c874c73
Miguel Arconada <miguelarman@gmail.com>
2019-05-13 23:56:21 +0200

    Borrados ficheros innecesaios

--- control_terminar.py
+++
@@ -1,9 +0,0 @@
-class ControlTerminar:
-    def __init__(self):
-        self.flag_terminar = False
-
-    def terminar(self):
-        return self.flag_terminar
-
-    def setTerminar(self, bool):
-        self.flag_terminar = bool
